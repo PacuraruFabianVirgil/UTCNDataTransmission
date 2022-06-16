38456683e69966420c23fe549747f83371b5fd94
Pacuraru Fabian Virgil <101675637+PacuraruFabianVirgil@users.noreply.github.com>
2022-06-16 23:37:27 +0300

    Delete Lab 1 directory

--- Lab 1/Lab1.3/appTest.js
+++
@@ -1,7 +0,0 @@
-function test(){
- console.log(sum(5)==15?"Passed":"Failed");
- console.log(sum('This works')==" not a number "?"Passed":"Failed");
- console.log(sum(true)==" not a number "?"Passed":"Failed");
- }
- test();
- 
--- Lab 1/Lab1.4/appTest.js
+++
@@ -1,8 +0,0 @@
-function test(){
- console.log(getFibonacci(2));
- console.log(getFibonacci(5));
- console.log(getFibonacci(" not good ")==" not allowed "?"Passed":"Failed");
- console.log(getFibonacci(11)==" not allowed "?"Passed":"Failed");
- }
- test();
- 
