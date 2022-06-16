efaf2d853fb8e76b088e699045ff97250692689b
Pacuraru Fabian Virgil <101675637+PacuraruFabianVirgil@users.noreply.github.com>
2022-06-16 23:36:13 +0300

    Laboratory1

---
+++ Lab 1/Lab1.4/appTest.js
@@ -0,0 +1,8 @@
+function test(){
+ console.log(getFibonacci(2));
+ console.log(getFibonacci(5));
+ console.log(getFibonacci(" not good ")==" not allowed "?"Passed":"Failed");
+ console.log(getFibonacci(11)==" not allowed "?"Passed":"Failed");
+ }
+ test();
+ 
